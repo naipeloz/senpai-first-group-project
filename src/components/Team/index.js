081645f7145ef70ef68358417363c125c301d03f
fedeg2411 <64233418+fedeg2411@users.noreply.github.com>
2021-07-17 19:05:17 -0300

    sadfasd
    asdfasd

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Team = ({name, coaches, players}) => {
+const Team = ({temperatura,ciudad,name}) => {
+    const [nombre,cambionombre] = useState(""); 
+    const [temp,cambiotemp] = useState(""); 
+    const [icono,cambioicono] = useState("");
+    const [humedad,cambiohumedad] = useState(""); 
   return (
     <div className="bg-gray-300 m-4 p-2 rounded">
-      <h1 className="text-lg font-bold">{name}</h1>
-      <h2>Entrenador: {coaches?.map(coach => coach.coach_name)}</h2>
-      <ul>
-        {players?.map(player => 
-          <li key={player.player_key}>
-            {<span className="font-bold">{player.player_type}</span>}
-            {` #${player.player_number} ${player.player_name}`}
-          </li>
-        )}
-      </ul>
+      <h1 className="text-lg font-bold">{}</h1>
+      <h2>nombre ciudad:{nombre} </h2>
+      <h3>Temperatura:{temp} </h3>
+      <h3>humedad:{temp} </h3>
+      <h1> <img src={icono}></img> </h1>
+      <button onClick={() => {cambionombre(ciudad.name); cambiotemp(temperatura.temperature); cambioicono(temperatura.weather_icons); cambiohumedad(temperatura.humidity)}}>Cargar</button>
+      
+      
     </div>
   )
 }
--- src/pages/Example.js
+++ src/pages/Example.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getStaffTeam } from '../api/example';
 import Team from '../components/Team';
 
-export const FetchExample = () => {
+export const  FetchExample = () => {
   const [matches, setMatches] = useState([]); 
   useEffect(() => {
     getStaffTeam()
@@ -10,18 +10,17 @@ export const FetchExample = () => {
         setMatches(response);
       })
       .catch();
+      
   }, []);
+
   return (
     <div className="container mx-auto">
-      {matches?.map(item => 
-        <Team
-          key={item.team_key}
-          name={item.team_name}
-          badge={item.team_badge}
-          coaches={item.coaches}
-          players={item.players}
-        />
-      )}
-    </div>
+    {<Team
+        temperatura={matches.current}
+        ciudad={matches.location}
+    />
+    }
+</div>
+   
   )
 }
