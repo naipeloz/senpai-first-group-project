9bc910bbd96820f2185d9496da40d54353597085
Federico <federico@monkis.uy>
2021-07-20 20:38:28 -0300

    AJustes con agus

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -7,7 +7,7 @@ const News = ({title, img, description,link,darkMode}) =>{
         <div className="w-1/4 p-2 rounded"> 
             <div className={`w-full ${darkMode ? 'bg-gray-300' : 'bg-white'}  rounded p-2`}> 
                 <img src={img} />
-                <h1 className="text-black font-bold" lassName="text-lg font-bold">{title}</h1>
+                <h1 className="text-black font-bold" className="text-lg font-bold">{title}</h1>
                 <p className="text-black">{description}</p>
                 <a href={link} target="_blank" className="text-black">ver más</a>
             </div>      
