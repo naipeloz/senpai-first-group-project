9234d40503e4c7715079151dee6ccfb4e4cfddb1
feramb626 <nando626@gmail.com>
2021-07-30 00:29:09 -0300

    ahora si funca

--- src/api/football.js
+++ src/api/football.js
@@ -1,7 +1,7 @@
 const API_KEY = '3f5f292cb66187502c21b62f83febd7ad93e7943caf4012a7cb7345352626f3c'
-const BASE_URL = 'https://apiv3.apifootball.com/?action=get_topscorers&league_id=302&APIkey='
+const BASE_URL = 'https://apiv3.apifootball.com/'
 
-const getTopScorers = () =>
-    fetch(`${BASE_URL}${API_KEY}`)
-    .then(response => response.jason());
+export const getTopScorers = () =>
+    fetch(`${BASE_URL}?action=get_topscorers&league_id=302&APIkey=${API_KEY}`)
+    .then(response => response.json());
     
--- src/pages/Football.js
+++ src/pages/Football.js
@@ -1,20 +1,19 @@
-import React, {useEffect, useSate} from 'react';
-import {getTopScorers} from '../api/football';
+import React, { useEffect, useState } from 'react';
+import { getTopScorers } from '../api/football';
 
 const ScorersPage = () => {
-
-    const [scorers, setScorers] = useSate([]);
-
+    const [Scorers, setScorers] = useState([]);
     useEffect(() => {
         getTopScorers()
         .then((response) => {
             if(response.error){
                 alert("No se pudo consultar")
             } else {
+                console.log(response)
                 const results = response;
                 setScorers(results)
             }  
-        })
+        });
     }, []);
 
 
